3ab4c3dca50613813c01b3ebbabae878c2795953
Federico Guidi <federicoguidi196@gmail.com>
2018-04-26 14:02:12 +0200

    Working version, rainbow mode and various UI improvements

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from wireless import wifi
 # change the following line to use a different wifi driver
 from espressif.esp8266wifi import esp8266wifi as wifi_driver
 import streams
+import threading
 
 # Import the Zerynth APP library
 from zerynthapp import zerynthapp
@@ -20,41 +21,92 @@ pinMode(D2,OUTPUT) # blue
 current_r = LOW
 current_g = LOW
 current_b = LOW
+on = False
+stopped = False
 
 # define a RPC function: generate a random number
 def turn_on():
+    global on
+    on = True
     digitalWrite(D0,current_r)
     digitalWrite(D1,current_g)
     digitalWrite(D2,current_b)
 
 def turn_off():
+    global on
+    on = False
     digitalWrite(D0,LOW)
     digitalWrite(D1,LOW)
     digitalWrite(D2,LOW)
     
 def red_led():
     if (current_r == LOW):
-        digitalWrite(D0, HIGH)
         current_r = HIGH
     else:
-        digitalWrite(D0, LOW)
         current_r = LOW
+    global on
+    if on:
+        digitalWrite(D0,current_r)
     
 def green_led():
     if (current_g == LOW):
-        digitalWrite(D1, HIGH)
         current_g = HIGH
     else:
-        digitalWrite(D1, LOW)
         current_g = LOW
+    global on
+    if on:
+        digitalWrite(D1,current_g)
 
 def blue_led():
     if (current_b == LOW):
-        digitalWrite(D2, HIGH)
         current_b = HIGH
     else:
-        digitalWrite(D2, LOW)
         current_b = LOW
+    global on
+    if on:
+        digitalWrite(D2,current_b)
+        
+def rainbow():
+    global stopped
+    stopped = False
+    def run():
+        while not stopped:
+            # purple
+            digitalWrite(D0,HIGH)
+            digitalWrite(D2,HIGH)
+            sleep(200)
+            # blue
+            digitalWrite(D0,LOW)
+            sleep(200)
+            # sky blue
+            digitalWrite(D1,HIGH)
+            sleep(200)
+            # green
+            digitalWrite(D2,LOW)
+            sleep(200)
+            # yellow
+            digitalWrite(D0,HIGH)
+            sleep(200)
+            # red
+            digitalWrite(D1,LOW)
+            sleep(200)
+            if stopped:
+                global on
+                if on:
+                    digitalWrite(D0,current_r)
+                    digitalWrite(D1,current_g)
+                    digitalWrite(D2,current_b)
+                else:
+                    digitalWrite(D0,LOW)
+                    digitalWrite(D1,LOW)
+                    digitalWrite(D2,LOW)
+                break
+    thread = threading.Thread(target=run)  
+    thread.start()
+
+def rainbow_stop():
+    global stopped
+    stopped = True
 
 
 # send events on button pressed
@@ -67,12 +119,14 @@ onPinFall(BTN0,on_btn,debounce=1000)
 # Device UID and TOKEN can be created in the ADM panel
 zapp = zerynthapp.ZerynthApp("_vb0ElDHQy-cfNsXUORK0Q", "h9aIzQJ3QsiQIPrSLQy4pA", log=True)
 
-# link "random" to do_random
+# link Python functions to Javascript
 zapp.on("turn_on", turn_on)
 zapp.on("turn_off",turn_off)
 zapp.on("RED", red_led)
 zapp.on("GREEN", green_led)
 zapp.on("BLUE", blue_led)
+zapp.on("RAINBOW", rainbow)
+zapp.on("STOP", rainbow_stop)
 
 try:
     # connect to the wifi network (Set your SSID and password below)
